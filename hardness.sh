6c52092180b39d1464e5a7524c8e11683daed66e
RedstoneOre <con@LAPTOP-DJ5RATMG.localdomain>
2025-07-19 23:18:31 +0800

    first

---
+++ hardness.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+declare -A hardness
+hardness['0']=0
+hardness['1']=1
+hardness['2']=2
+hardness['3']=4
+hardness['4']=8
+hardness['5']=16
+hardness['6']=32
+hardness['7']=64
+hardness['8']=8
+hardness['9']=9
+hardness['#']=5
+hardness['$']=50
+hardness["'"]=5
+hardness['"']=10
+hardness[OOT]=100
+hardness[BOL]=200
+defaulthardness=2
+function getHardness {
+	echo -n "${hardness["$1"]:-$defaulthardness}"
+}
