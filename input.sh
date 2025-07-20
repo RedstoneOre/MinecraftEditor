d33d45e6abd554ba5152279428c200b4eece2fce
RedstoneOre <con@LAPTOP-DJ5RATMG.localdomain>
2025-07-20 12:58:53 +0800

    input thread

---
+++ input.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+# Use fd 12 as te lock
+inputtmpfifo="$dirp"/tmp/$$.fifo
+mkfifo "$inputtmpfifo"
+exec 12<> "$inputtmpfifo"
+rm "$inputtmpfifo"
+function InputThread {
+	while read inputop <&12;do
+		[ "$inputop" == end ] && break
+		read -N 1 -t 0.5 op
+		case "$op" in
+			w) echo 'Operate_MoveUpwards' ;;
+			a) echo 'Operate_MoveLeft' ;;
+			s) echo 'Operate_MoveDownwards' ;;
+			d) echo 'Operate_MoveRight' ;;
+			i) echo 'Operate_MoveFocusUpwards' ;;
+			j) echo 'Operate_MoveFocusLeft' ;;
+			k) echo 'Operate_MoveFocusDownwards' ;;
+			l) echo 'Operate_MoveFocusRight' ;;
+			' ') echo 'Operate_Jump' ;;
+			'[') echo 'Operate_Dig' ;;
+			*) echo 'Operate_Nothing' ;;
+		esac
+		read -N 2147483647 -t 0.03
+	done
+}
---
+++ operate.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+function Operate_MoveUpwards {
+	[ `getCharOnPos "$px" "$[py-1]"` != BOL ] && {
+		move 0 -1; ismove=1
+		opsuc=1
+	}
+}
+function Operate_MoveLeft {
+	move -1 0; ismove=1;opsuc=1
+}
+function Operate_MoveDownwards {
+	move 0 1; ismove=1;opsuc=1
+}
+function Operate_MoveRight {
+	move 1 0; ismove=1;opsuc=1
+}
+function Operate_Jump {
+	[ "$power" -ge 20 ] && {
+		[ `getCharOnPos "$px" "$[py-2]"` != BOL ] && move 0 -2
+		power="$[power-20]"
+		opsuc=1
+	}
+}
+
+function Operate_MoveFocusUpwards {
+	movedig 0 -1; opsuc=1
+}
+function Operate_MoveFocusLeft {
+	movedig -1 0; opsuc=1
+}
+function Operate_MoveFocusDownwards {
+	movedig 0 1; opsuc=1
+}
+function Operate_MoveFocusRight {
+	movedig 1 0; opsuc=1
+}
+
+function Operate_Nothing {
+	opsuc=1
+}
+function Operate_Dig {
+	dig "$dix" "$diy" && {
+		opsuc=1;isdig=1
+	}
+}
