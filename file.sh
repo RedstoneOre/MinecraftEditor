d40118a7ca497754eb40861b0d9f171a7db453c0
RedstoneOre <con@LAPTOP-DJ5RATMG.localdomain>
2025-07-21 01:20:30 +0800

    espace 1~127

--- file.sh
+++ file.sh
@@ -2,6 +2,7 @@
 [ -v MCEDITOR_INC_file ] || {
 	[ "$debug" -ge 1 ] && echo 'File loading header loaded'
 	MCEDITOR_INC_file=
+	. "$dirp"/espmap.sh
 	# Read_File <Dimension> < (standard input)
 	#  Read a file into a dimension
 	function Read_File {
@@ -10,12 +11,8 @@
 			read fc[$i] || break
 			fc["$rdim.$i.0"]='BOL'
 			for((j=1;;));do
-				read -N 1 tfc || break
-				tfcres=()
-				case "$tfc" in
-					$'\t') tfcres=( SOT POT POT POT POT POT POT EOT ) ;;
-					*) tfcres=( "$tfc" ) ;;
-				esac
+				read -n 1 -d $'\0' tfc || break
+				mceespace "$tfc"
 				for((k=0;k<"${#tfcres[@]}";++k));do
 					fc["$rdim.$i.$j"]="${tfcres[k]}"
 					j="$[j+1]"
