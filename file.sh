c814d58e60ab29c8f392f35aa11fd5fee1f241fa
RedstoneOre <user@LAPTOP-DJ5RATMG.localdomain>
2025-07-29 19:00:28 +0800

    file saving support

--- file.sh
+++ file.sh
@@ -35,4 +35,62 @@
 		local i=
 		for i;do echo -n "$i"; done
 	}
+	# Save_File <Dimension> > (output)
+	#  Save a dimension to a file
+	function Save_File {
+		local unkespmsg='(What is this fucking char?)'
+		ldim=$dim
+		dim="${1:-0}"
+		heap_copy fcm$dim fcmsave
+		local charp= char= espst=0 espc=() esps= esprs=
+		while [ `heap_getsize fcmsave` -gt 0 ]; do
+			charp=`heap_gettop fcmsave`
+			char=`getChar ${charp//.*/} ${charp//*./}`
+			heap_pop fcmsave
+			# echo -n "[$espst]"
+			case "$espst" in
+				0) 
+					case "${char:-OOT}" in
+						BOL) echo ;;
+						OOT) ;;
+						ESP) espst=1 ;;
+						ELB) espst=2 ;;
+						ERB) ;;
+						SOT) espst=3 ;;
+						POT) echo -n $'\t' ;;
+						EOT) echo -n $'\t' ;;
+						*) echo -n "$char" ;;
+					esac ;;
+				1)
+					esprs="${unesp["e$char"]:-"$unkespmsg"}"
+					[ "$esprs" == 'NUL' ] && {
+						echo -n $'\0'
+					} || {
+						echo -n "$esprs"
+					}
+					espst=0 ;;
+				2)
+					[ "$char" == 'ERB' ] && {
+						esps=`_file_connect_str "${espc[@]}"`
+						esprs="${unesp["e$esps"]:-"$unkespmsg"}"
+						[ "$esprs" == 'NUL' ] && {
+							echo -n $'\0'
+							true
+						} || {
+							echo -n "$esprs"
+						}
+						espst=0 espc=() esps=
+						true
+					} || {
+						espc[${#espc[@]}]="$char"
+					} ;;
+					3)
+						[ "$char" == 'EOT' ] && {
+							echo -n $'\t'
+							espst=0
+						}
+				esac
+		done
+		dim=$ldim
+	}
 }
