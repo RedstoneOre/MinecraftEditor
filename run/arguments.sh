d273ca388e0f5b98289762ad37ed72433f2b3826
RedstoneOre <microRedstoneOre@gmail.com>
2025-08-11 19:53:25 +0800

    awa

---
+++ run/arguments.sh
@@ -0,0 +1,8 @@
+
+#! /bin/bash
+[ -v MCEDITOR_INC_arguments ] || {
+	[ "$MCEDITOR_dbgl" -ge 2 ] && echo 'Argument parsing loaded'
+	MCEDITOR_INC_arguments=
+
+	
+}
---
+++ run/lang.sh
@@ -0,0 +1,7 @@
+
+#! /bin/bash
+[ -v MCEDITOR_INC_lang ] || {
+	[ "$MCEDITOR_dbgl" -ge 2 ] && echo 'language header loaded'
+	MCEDITOR_INC_lang=
+	function 
+}
